61b4b7ea09367108a930f119a7ff5e1c2f42b7c1
mariolivera <113113372+mariolivera@users.noreply.github.com>
2022-09-23 11:41:02 -0300

    atualizado 23/09

--- modulosJSale/aula-objetos/exercicio-todolist/eventos.js
+++ modulosJSale/aula-objetos/exercicio-todolist/eventos.js
@@ -1,16 +1,41 @@
+let lista_tarefas = []; /* aqui é a lista de tarefas que o usuario anexa */
+
+function atualizarQuantidade(){
+    document.getElementById('numeros').innerHTML = lista_tarefas.length;
+}
+
+function listarTarefas(){   /* função que recebe todas as variaveis do ususario e oganizando com o map */
+    let conteudo = lista_tarefas.sort().map(function(tarefa){
+        return `
+    <div>
+        <input type="checkbox"> ${tarefa}
+    </div>
+`; 
+    })
+    document.getElementById('tarefas').innerHTML = conteudo.join(''); /* aqui vai receber as variaveis que vão receber todas as variaveis que vão ser colocada nas divs da input.*/
+}
+
 function addtarefa(){
     event.preventDefault();
     
+let titulo = document.getElementById('input_nova_tarefa').value; 
+if (titulo.trim() === ''){
+    alert('tarefa invalida');
+    return
+}
+if(true === lista_tarefas.includes(titulo)){
+    alert('tarefa já existe')
+    return
+}
 
 
-let titulo = document.getElementById('input_nova_tarefa').value;
+lista_tarefas.push(titulo);
+
 
 document.getElementById('input_nova_tarefa').value ='';
 
-document.getElementById('tarefas').innerHTML += `
-    <div>
-        <input type="checkbox"> ${titulo}
-    </div>
-`;
+
+    listarTarefas(); /* chama a função listarTarefas lá de cima. */
+    atualizarQuantidade(); /* chama a função atualizarQuantidade lá de cima. */
 
 }
